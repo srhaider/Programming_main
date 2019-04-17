011d13b7aaca0b9b3db538c2f229d9aa58609616
Rizwan Haider <srhaider@outlook.in>
2019-04-17 12:24:03 -0700

    First commit from Python Branch

---
+++ Pyhthon_Programming/PythonPractice/FirstPython_Script.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+b=15
+c= ~b
+print c<<2
+
+
+
+
+
+'''
+Dict={}
+Dict['one']="Hello Mr"
+Dict[2]=" Murabn"
+
+print Dict['one']
+
+ComptDict={'name': Dict['one'] + Dict[2], 'code': 112, 'Dept': 'Ed'}
+print ComptDict.values()
+'''
+
+
+
+
+'''
+li = ['Hello',12.3,123,'dragon']
+print li[1:]*2
+'''
+
+"""
+if 1>2:
+	print "1 is greater Than 2"
+else:
+	print "2 is greater thann 1"
+"""
+
+
+"""
+import sys
+
+try:
+	#open a file
+	file = open(Alpha_X,"w")
+except IOError:
+	print "There was an error writing too", Alpha_X
+	sys.exit()
+
+"""
+'''
+Name="Jain"
+str= "Alpha" + Name
+print "Alpha"+Name * 2
+'''
+
+
+
