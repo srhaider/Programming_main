34856368f4e719d3e2dc3a17d00fbb2465541430
Rizwan Haider <srhaider@outlook.in>
2019-04-20 12:36:04 -0700

    C Programming temporar file is created

---
+++ C_Programming/Practice_Program/Temp1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+#define TAG (newline='\n', WideSpace='\t');
+#define enum TAG	{newline='\n',WideSpace='\t' };
+int main()
+{
+int a=1;
+char c='A';
+
+
+if (0&&1)
+printf("And Got executed\n");
+else
+
+printf("And Not executed\n");
+
+
+//printf("integer value of character 'A' =%c\t\n",a);
+//printf("integer value of a =%d\t\n",a);
+//printf("integer value of c =%c\t\n",c);
+return 0;
+}
