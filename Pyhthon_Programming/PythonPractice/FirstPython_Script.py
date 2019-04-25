98ceddba31a5ab3c49aa4b08e51a181440266381
Rizwan Haider <srhaider@outlook.in>
2019-04-25 12:22:19 -0700

    Basic Pyhton code

--- Pyhthon_Programming/PythonPractice/FirstPython_Script.py
+++ Pyhthon_Programming/PythonPractice/FirstPython_Script.py
@@ -1,10 +1,155 @@
 #!/usr/bin/python
+#Filename : FirstPython_Script.py 
+import sys
 
+if __name__ == '__main__':
+	'''Hello 1st line
+	Hello 2nd line'''
+	for i in sys.argv:
+		print i
+	'''Ye Kya ho 
+	raha hai bhai'''
+
+	
+print main.__doc__
+
+'''
+For Loo Decrement
+for a in range (10,0,-1):
+	print a
+'''
+
+
+'''
+def main():
+	print "Hello Main"
+
+if __name__== ' main ':
+	main()
+'''
+
+
+'''
+cmp='d'
+
+def cmpareFun(cmp):
+#	global cmp
+	if cmp == raw_input('Enter Some No:'):
+		print "Entered Character is equivalent to given Charavter"
+	else:
+		print "Not Equal Character when Comp with Int inut"
+
+
+cmpareFun('c')
+print cmpareFun.__doc__
+help(cmpareFun)
+'''
+
+
+
+
+'''
+if cmp ==no:
+	print "Equal Charavter"
+else:
+	print "Not Equal Character"
+
+'''
+
+
+''' For Loop
+for a in range (1,10):
+	print a*2 
+	if a*2<20:
+		continue
+	else:
+		break
+'''
+
+''' Loop
+for a in range (1,6):
+	for i in range(a):
+		print "*"
+	print " "
+
+'''
+
+
+'''
+print("Second Number Pattern ")
+lastNumber = 6
+for row in range(1, lastNumber):
+	for column in range(1, row + 1):
+		print(column, end=' ')
+	print("")
+
+'''
+
+'''
+while True:
+	s in range(5,0,-1):
+		print s
+print 'Done'
+'''
+
+
+
+'''
+a=1
+
+for a in range(5, 0 , -1):
+	print a+4
+'''
+
+
+
+
+
+
+'''
+a=7
+count=0
+while a<10:
+	print a
+	a=a+1
+
+'''
+
+
+
+
+
+'''
+a=23
+b=19
+c=20
+
+#GreaterNo= 10 if a >b and b <c else 100
+#GreaterNo = 11 if a>b and b>c print "Hello" elif b>c and c>a print "No Hello" else print "3rd Print"
+GreaterNo = 12 if a>b and b>c else a
+print GreaterNo
+'''
+
+
+'''
+if b>c:
+	print "B is greater than C"
+	if c >a :
+		print "B is the Greatest no among A,B,C"
+	else:
+		print "C is the 2nd greatest NO"
+else:
+	print "A is the greatest No"
+'''
+
+
+
+'''
 b=15
 c= ~b
 print c<<2
 
-
+'''
 
 
 
@@ -23,6 +168,7 @@ print ComptDict.values()
 
 
 '''
+#LIST
 li = ['Hello',12.3,123,'dragon']
 print li[1:]*2
 '''
